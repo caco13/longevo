607f48fe1d26007105872c022781f26d8283e22d
caco <caco@member.fsf.org>
2016-09-22 15:36:11 -0300

    Cria formulário de filtro de chamados

---
+++ resources/views/chamados/index.blade.php
@@ -0,0 +1,31 @@
+@extends('app')
+
+@section('content')
+    <h1>Chamados</h1>
+    <hr>
+    @include('partials.filter')
+    <hr>
+
+    @if($chamados->isEmpty())
+        <p>A lista de chamados está vazia.</p>
+    @else
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Chamado</th>
+                    <th>Pedido</th>
+                    <th>Cliente</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($chamados as $chamado)
+                    <tr>
+                        <td>{{ $chamado->id }}</td>
+                        <td>{{ $chamado->pedido->id }}</td>
+                        <td>{{ $chamado->pedido->cliente->nome }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+@endsection
--- resources/views/partials/filter.blade.php
+++ resources/views/partials/filter.blade.php
@@ -1,3 +1,15 @@
 <section>
-
+    {!! Form::open(['url' => 'chamados']) !!}
+    <div class="form-group">
+        {!! Form::label('pedido', 'Pedido:') !!}
+        {!! Form::text('pedido', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::text('email', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Filtrar', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
+    {!! Form::close() !!}
 </section>
